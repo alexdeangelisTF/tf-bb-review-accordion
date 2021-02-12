0131feae54d9e249353d47611a542b3bee5db817
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-12 15:19:39 +0000

    toggle display of accordion on click on span element

--- tf-review-accordion/includes/frontend.js.php
+++ tf-review-accordion/includes/frontend.js.php
@@ -6,5 +6,8 @@
 
 (function($) {
 
+$('.tf-review-accordion .accordion-open span').on('click',function(){
+	$(this).parent().parent().find('.review-accordion-extra').slideToggle();
+})
 
 })( jQuery );
