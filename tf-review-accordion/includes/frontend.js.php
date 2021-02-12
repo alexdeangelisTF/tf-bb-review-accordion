221919491e247ec2991e4ef49e09555bd93aa173
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-12 15:28:47 +0000

    give open/close classes on accordion toggle

--- tf-review-accordion/includes/frontend.js.php
+++ tf-review-accordion/includes/frontend.js.php
@@ -5,9 +5,15 @@
 ?>
 
 (function($) {
-
-$('.tf-review-accordion .accordion-open span').on('click',function(){
-	$(this).parent().parent().find('.review-accordion-extra').slideToggle();
-})
+	
+	// On click of accordion open element
+	$('.tf-review-accordion .accordion-open span').on('click',function(){
+		// Toggle the display of the accordion extra element
+		$(this).parent().parent().find('.review-accordion-extra').slideToggle();
+		
+		$(this).parent().toggleClass('closed');
+		$(this).parent().toggleClass('open')
+		
+	})
 
 })( jQuery );
