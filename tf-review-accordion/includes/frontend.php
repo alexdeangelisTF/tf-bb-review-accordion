fc7919697a271e1a02bf78d19cc60f40d6753d02
Alex De Angelis <alex.deangelis@thinkingfox.com>
2021-02-12 15:05:57 +0000

    create 2 review loops
    The first loop contains the first review in the repeater field, the second loop contains all other reviews, wrapped in a special div

--- tf-review-accordion/includes/frontend.php
+++ tf-review-accordion/includes/frontend.php
@@ -12,24 +12,42 @@ if ($repeater_field_setting) {
 		$sub_field_1_setting = $settings->tf_review_accordion_sub_field_1;
 		$sub_field_2_setting = $settings->tf_review_accordion_sub_field_2;
 		
-		echo '<div class="tf-review-accordion">';
+		echo '<div class="tf-review-accordion tf-review-accordion-' . $id . '>">';
 		
 		// If sub field 1 or 2 settings exist, then run the loop for the repeater field
 		if ($sub_field_1_setting || $sub_field_2_setting) {
 			
 			$review_count = 0;
 			foreach($repeater_field as $review) {
-				
-				if ($sub_field_1_setting) {
-					$sub_field_1 = $review[$sub_field_1_setting];
+				if ($review_count == 0) {  
+					if ($sub_field_1_setting) {
+						$sub_field_1 = $review[$sub_field_1_setting];
+					}
+					if ($sub_field_2_setting) {
+						$sub_field_2 = $review[$sub_field_2_setting];
+					}
+					include( plugin_dir_path( __FILE__ ) . '/review_body/body.php');
 				}
-				if ($sub_field_2_setting) {
-					$sub_field_2 = $review[$sub_field_2_setting];
-				}
-				include( plugin_dir_path( __FILE__ ) . '/review_body/body.php');
 				$review_count++;
-				
 			}
+			if ($review_count > 0) {
+				$review_count = 0;
+				echo '<div class="review-accordion-extra">';
+				foreach($repeater_field as $review) {
+					if ($review_count != 0) {
+						if ($sub_field_1_setting) {
+							$sub_field_1 = $review[$sub_field_1_setting];
+						}
+						if ($sub_field_2_setting) {
+							$sub_field_2 = $review[$sub_field_2_setting];
+						}
+						include( plugin_dir_path( __FILE__ ) . '/review_body/body.php');
+					}
+					$review_count++;
+				}
+				echo '</div>';
+			} 
+			
 			
 		}
 		
